b18351da30b8c3773178faa0dd896e1acd3c130e
Muhammed Kanyi <kanyi@gcubed.gm>
2023-11-02 10:39:17 +0000

    updated the api endpoint

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,4 @@
-// let apiEndpoint = `https://api.petshelpful.com/`;
-let apiEndpoint = `http://localhost:4000`;
+let apiEndpoint = `https://api.petshelpful.com/`;
+// let apiEndpoint = `http://localhost:4000`;
 
 export default apiEndpoint;
