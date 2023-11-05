da6dbc696243ba92b3219e927c84a10c0df7bad8
Muhammed Kanyi <kanyi@gcubed.gm>
2023-11-05 18:20:46 +0000

    added storage to shared

--- src/Pages/Ads/components/RightSideContent.js
+++ src/Pages/Ads/components/RightSideContent.js
@@ -76,80 +76,15 @@ const {t} =useTranslation();
           </CardBody>
         </Card>
 
-        {/* <Card className="company-profile mt-4">
-          <CardBody className="p-4">
-            <div className="text-center">
-              <img src={jobImages2} alt="" className="img-fluid rounded-3" />
-
-              <div className="mt-4">
-                <h6 className="fs-17 mb-1">Jobcy Technology Pvt.Ltd</h6>
-                <p className="text-muted">Since July 2017</p>
-              </div>
-            </div>
-            <ul className="list-unstyled mt-4">
-              <li>
-                <div className="d-flex">
-                  <i className="uil uil-phone-volume text-primary fs-4"></i>
-                  <div className="ms-3">
-                    <h6 className="fs-14 mb-2">Phone</h6>
-                    <p className="text-muted fs-14 mb-0">+589 560 56555</p>
-                  </div>
-                </div>
-              </li>
-              <li className="mt-3">
-                <div className="d-flex">
-                  <i className="uil uil-envelope text-primary fs-4"></i>
-                  <div className="ms-3">
-                    <h6 className="fs-14 mb-2">Email</h6>
-                    <p className="text-muted fs-14 mb-0">
-                      pixltechnology@info.com
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li className="mt-3">
-                <div className="d-flex">
-                  <i className="uil uil-globe text-primary fs-4"></i>
-                  <div className="ms-3">
-                    <h6 className="fs-14 mb-2">Website</h6>
-                    <p className="text-muted fs-14 text-break mb-0">
-                      www.Jobcytechnology.pvt.ltd.com
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li className="mt-3">
-                <div className="d-flex">
-                  <i className="uil uil-map-marker text-primary fs-4"></i>
-                  <div className="ms-3">
-                    <h6 className="fs-14 mb-2">Location</h6>
-                    <p className="text-muted fs-14 mb-0">
-                      Oakridge Lane Richardson.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </ul>
-            <div className="mt-4">
-              <Link
-                to="/companydetails"
-                className="btn btn-primary btn-hover w-100 rounded"
-              >
-                <i className="mdi mdi-eye"></i> View Profile
-              </Link>
-            </div>
-          </CardBody>
-        </Card> */}
-
         <div className="mt-4">
-          <h6 className="fs-16 mb-3">Job location</h6>
-          <iframe
-            title="maps"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830869428!2d-74.119763973046!3d40.69766374874431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1628067715234!5m2!1sen!2sin"
-            style={{ width: `100%`, height: `250` }}
-            allowFullScreen=""
-            loading="lazy"
-          ></iframe>
+          <h6 className="fs-16 mb-3">Ad Location</h6>
+            <iframe
+                width="100%"
+                height="200"
+                frameBorder="0"
+                style={{ border: 0 }}
+                src={`https://www.google.com/maps/embed/v1/view?key=AIzaSyBeBlcI_0OO_dK5bJKHSGKSfQUqDivb0Ro&center=${pet.latitude},${pet.longitude}&zoom=15`}
+            ></iframe>
         </div>
         <div
           className="modal fade"
