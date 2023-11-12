415280148cd064d65f4a9593847433cee10d6796
Muhammed Kanyi <kanyi@gcubed.gm>
2023-11-12 14:05:36 +0000

    added delete

--- src/Pages/Profile/ManageAds/components/AdsList.js
+++ src/Pages/Profile/ManageAds/components/AdsList.js
@@ -135,12 +135,12 @@ const JobListing = () => {
                         data-bs-placement="top"
                         title="Edit"
                       >
-                        <Link
+                        {/* <Link
                           to="/bookmarkjobpost"
                           className="avatar-sm bg-success-subtle text-success d-inline-block text-center rounded-circle fs-18"
                         >
                           <i className="uil uil-edit"></i>
-                        </Link>
+                        </Link> */}
                       </li>
                       <li
                         className="list-inline-item"
