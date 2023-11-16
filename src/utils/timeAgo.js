c04d56ac65236b884057d7bf6a6e8b4f634c8e85
Muhammed Kanyi <kanyi@gcubed.gm>
2023-11-16 03:37:30 +0000

    fixed the time ago translation in english

--- src/utils/timeAgo.js
+++ src/utils/timeAgo.js
@@ -5,20 +5,20 @@ function timeAgo(dateString, translations) {
     const secondsPast = (now - date) / 1000;
 
     if (secondsPast < 60) {
-        return translations.secondsAgo(Math.round(secondsPast));
+        return secondsPast + ' ' + translations.secondsAgo(Math.round(secondsPast));
     }
     if (secondsPast < 3600) {
-        return translations.minutesAgo(Math.round(secondsPast / 60));
+        return secondsPast + ' ' + translations.minutesAgo(Math.round(secondsPast / 60));
     }
     if (secondsPast <= 86400) {
-        return translations.hoursAgo(Math.round(secondsPast / 3600));
+        return secondsPast + ' ' +translations.hoursAgo(Math.round(secondsPast / 3600));
     }
     if (secondsPast > 86400) {
         const day = Math.round(secondsPast / 86400);
         if (day === 1) {
             return translations.yesterday;
         } else {
-            return translations.daysAgo(day);
+            return day + ' ' + translations.daysAgo(day);
         }
     }
 }
